c831498171a92d8b16a8161a1f6e38cdc23c6779
Shahana Salam <shahana.salam@cafu.com>
2025-01-12 11:43:36 +0400

    add categorized bookmarks

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   chrome.bookmarks.getTree((bookmarkTreeNodes) => {
-    console.log("All browser bookmarks:", bookmarkTreeNodes);
+    const categorizedBookmark = getCategorizedBookmark(bookmarkTreeNodes);
+    console.log(categorizedBookmark);
   });
 });
+
+const getCategorizedBookmark = (bookmarkNodes) => {
+  const result = [];
+  bookmarkNodes[0].children.map((allbookmark) => {
+    allbookmark.children.map((bookmark) => {
+      if (bookmark.children) {
+        result.push(bookmark);
+      }
+    });
+  });
+
+  return result;
+};
