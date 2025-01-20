73a63948c8e18e5458cb53556254bc8fde2be8ea
Shahana Salam <shahana.salam@cafu.com>
2025-01-20 22:37:44 +0400

    add service wroker

--- src/content.js
+++ src/content.js
@@ -1,1 +1,9 @@
-console.log("Content script running!");
+console.log("ss");
+chrome.runtime.sendMessage("get-user-data", (response) => {
+  if (chrome.runtime.lastError) {
+    console.error("Error:", chrome.runtime.lastError.message);
+  } else {
+    console.log("Received user data:", response);
+    initializeUI(response);
+  }
+});
